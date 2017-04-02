300a06d1b0a10c830322644b9d8e48d3f4569d76
Mohd Feroz Baig <feroz.baig96@gmail.com>
2017-04-02 23:00:54 +0530

    State

---
+++ src/State/Execute.java
@@ -0,0 +1,16 @@
+package State;
+
+public class Execute {
+
+    public static void main(String[] args) {
+
+        ATMMachine atmMachine = new ATMMachine();
+        atmMachine.insertCard();
+        atmMachine.ejectCard();
+        atmMachine.insertCard();
+        atmMachine.insertPin(1234);
+        atmMachine.requestCash(2000);
+        atmMachine.insertCard();
+        atmMachine.insertPin(1234);
+    }
+}
