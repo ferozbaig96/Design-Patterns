28c66ea7f188c993eff316f5378dec653e32d540
Mohd Feroz Baig <feroz.baig96@gmail.com>
2017-04-05 21:52:44 +0530

    Mediator

---
+++ src/Mediator/Execute.java
@@ -0,0 +1,28 @@
+package Mediator;
+
+public class Execute {
+
+    /*
+        The Mediator design pattern is used to handle communication
+        between related objects (Colleagues). All communication is
+        handled by a Mediator Object and the Colleagues don’t need
+        to know anything about each other to work together.
+    */
+
+
+    public static void main(String[] args) {
+        StockMediator stockMediator = new StockMediator();
+
+        GormanSlacks broker1 = new GormanSlacks(stockMediator);
+        JTPoorman broker2 = new JTPoorman(stockMediator);
+
+        broker1.saleOffer("MSFT", 100);
+        broker1.saleOffer("GOOG", 50);
+
+        broker2.saleOffer("MSFT", 100);
+        broker2.saleOffer("NRG", 10);
+
+        broker1.buyOffer("NRG", 10);
+        stockMediator.showStockOfferings();
+    }
+}
